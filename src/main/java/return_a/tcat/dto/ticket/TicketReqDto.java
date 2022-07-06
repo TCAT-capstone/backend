647117b2c29631f9f184495bb9459a89eaa6724c
Kang Minji <mminjg@gmail.com>
2022-07-06 23:24:00 +0900

    :art: 요청 camel case로 변경
    기존 sake case로 받는 것에서 camel case로 변경하였습니다.

--- src/main/java/return_a/tcat/dto/ticket/TicketReqDto.java
+++ src/main/java/return_a/tcat/dto/ticket/TicketReqDto.java
@@ -13,7 +13,6 @@ import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class TicketReqDto {
 
     private Long memberId;
