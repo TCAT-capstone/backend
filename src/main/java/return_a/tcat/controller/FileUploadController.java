07c4b2601887e86e0bb2300553f6ecaf195e228a
Kang Minji <mminjg@gmail.com>
2022-07-13 18:51:45 +0900

    :recycle: 이미지 url 응답 객체 형태로 변경

--- src/main/java/return_a/tcat/controller/FileUploadController.java
+++ src/main/java/return_a/tcat/controller/FileUploadController.java
@@ -1,11 +1,14 @@
 package return_a.tcat.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import return_a.tcat.dto.file.ImageDto;
 import return_a.tcat.service.FileUploadService;
 
 import java.io.IOException;
@@ -18,8 +21,8 @@ public class FileUploadController {
     private final FileUploadService fileUploadService;
 
     @PostMapping("/images")
-    public String upload(@RequestParam("images") MultipartFile multipartFile) throws IOException {
-        String fileUrl=fileUploadService.upload(multipartFile, "static");
-        return fileUrl;
+    public ResponseEntity<ImageDto> upload(@RequestParam("images") MultipartFile multipartFile) throws IOException {
+        String fileUrl = fileUploadService.upload(multipartFile, "static");
+        return ResponseEntity.status(HttpStatus.OK).body(new ImageDto(fileUrl));
     }
 }
