bbd84c1e984a73c39d6dd3d1f13b9293636bce18
Kang Minji <mminjg@gmail.com>
2022-08-16 18:39:24 +0900

    :bulb: 주석처리

--- src/test/java/return_a/tcat/TcatApplicationTests.java
+++ src/test/java/return_a/tcat/TcatApplicationTests.java
@@ -3,7 +3,7 @@ package return_a.tcat;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class TcatApplicationTests {
 
 	@Test
