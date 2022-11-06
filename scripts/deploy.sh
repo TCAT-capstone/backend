e040b1b17a29f3828999c8c9a338176fe79af4b3
Kang Minji <mminjg@gmail.com>
2022-11-06 18:36:35 +0900

    :rocket: blue-green 배포방식 적용

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-echo "> 현재 실행 중인 Docker 컨테이너 pid 확인" >> /home/ec2-user/deploy.log
-CURRENT_PID=$(sudo docker container ls -q)
+DOCKER_APP_NAME=spring
 
-if [ -z $CURRENT_PID ]
-then
-  echo "> 현재 구동중인 Docker 컨테이너가 없으므로 종료하지 않습니다." >> /home/ec2-user/deploy.log
+EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+
+if [ -z "$EXIST_BLUE" ]; then
+	echo "blue up"
+	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
+
+	sleep 10
+
+	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+	docker image prune -af # 사용하지 않는 이미지 삭제
+
 else
-  echo "> sudo docker stop $CURRENT_PID"   # 현재 구동중인 Docker 컨테이너가 있다면 모두 중지
-  sudo docker stop $CURRENT_PID
-  sleep 5
-fi
+	echo "green up"
+	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
 
-cd /home/ec2-user/app
-sudo docker build -t tcat-api-spring-boot-docker .
-sudo docker run -d -p 8080:8080 tcat-api-spring-boot-docker
+	sleep 10
+
+	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+	docker image prune -af
+fi
